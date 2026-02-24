fc73657b96497323264ed7fff96fae9202ef44bc
DANI08XO <254754632+DANI08XO@users.noreply.github.com>
2026-02-24 00:44:25 -0800

    Added Intro | Update #1

--- src/ContagionSimulationMain.java
+++ src/ContagionSimulationMain.java
@@ -2,7 +2,11 @@
 // Codevid-19 Group: Daniel, Kenzuki, Helen, Kushal
 
 public class ContagionSimulationMain {
+
     public static void main (String args[]) {
-        System.out.print("OVOXO");
+        System.out.print("Welcome to Codevid-19 final project for CS 142");
+        System.out.print("Our project will focus on simulating a disease spread");
+        System.out.print("The user will be able to control how deadly and contagious the disease is along with the populagittion for the simulation");
     }
+
 }
