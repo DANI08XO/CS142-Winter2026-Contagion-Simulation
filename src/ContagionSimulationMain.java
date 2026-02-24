cf2d675b7e12562ae3e0f941c85c91075ab4de3e
DANI08XO <danilacruz98612@gmail.com>
2026-02-24 00:12:58 -0800

    Initial commit

---
+++ src/ContagionSimulationMain.java
@@ -0,0 +1,8 @@
+// Final Project for CS 142: Contagion Simulation
+// Codevid-19 Group: Daniel, Kenzuki, Helen, Kushal
+
+public class ContagionSimulationMain {
+    public static void main (String args[]) {
+        System.out.print("OVOXO");
+    }
+}
