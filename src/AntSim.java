1427bb2b747e973b52cc82793a5bbfec03887bb5
KyleHamasaki <hamhammmerham@gmail.com>
2026-02-23 19:13:09 -0800

    Update AntSim.java
    I wanted to see how writing code on GitHub works. Please comment something in AntSim to let me know you can see the changes. Thanks.

--- src/AntSim.java
+++ src/AntSim.java
@@ -1,5 +1,17 @@
 // AntSim.java
 
 public class AntSim {
+    public static void main(string[] args) {
+        printIntro();
+
+
+    }
+    // Prints the introduction
+    // Feel free to change the introduction
+    public static void printIntro() {
+        System.out.println("Welcome to the Ant Colony Simulator!");
+        System.out.println("This program creates a world with ant colonies and simulates ants'");
+        System.out.println("behaviors like foraging, fighting, and procreating.")
+    }
 
 }
